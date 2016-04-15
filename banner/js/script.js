7904aeeaf9c06cccadd56a54f77ba8155c7da3ba
NatashaByrkovetskaya <meduza15@bigmir.net>
2016-04-15 12:33:21 +0300

    banner

--- banner/js/script.js
+++ banner/js/script.js
@@ -1,28 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-   function moveText(){
-       var banner = $(".text1");
-       var banner2 = $(".text");
+    function moveText() {
+        var banner = $(".text1");
+        var banner2 = $(".text");
 
-       banner.animate({left: 40}, 1000);
-       banner.delay(1700).animate({opacity: 0});
-       banner2.delay(3000).animate({left: 40}, 1000);
-   }
-  moveText();
-    //function movePictures(){
-    //    var $element = $(".cookies");
-    //    var lastHeight = $(".cookies1").css('height');
-    //    function checkForChanges()
-    //    {
-    //        if ($element.css('height') != lastHeight)
-    //        {
-    //            lastHeight = $element.css('height');
-    //        }
-    //
-    //        setTimeout(checkForChanges, 500);
-    //    }
-    //    checkForChanges();
-    //
-    //}
-    //movePictures();
+        banner.animate({left: 40}, 750);
+        banner.delay(1500).animate({opacity: 0});
+        banner2.delay(2500).animate({left: 40}, 750);
+    }
+
+    moveText();
+    function movePictures() {
+        $("#cookie1").delay(1500).fadeIn();
+        $("#cookie2").delay(1750).fadeIn();
+        $("#cookie3").delay(2000).fadeIn();
+    }
+
+    movePictures();
 });
